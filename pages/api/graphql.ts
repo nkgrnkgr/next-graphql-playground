e00ed3f49a5fa3216ea505b2036006e8b66ae4fc
taiki-nishi <taiki-nishi@cybozu.co.jp>
2022-10-05 15:51:58 +0900

    update

---
+++ pages/api/graphql.ts
@@ -0,0 +1,42 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { ApolloServer } from "apollo-server-micro";
+import { readFileSync } from "fs";
+import { join } from "path";
+
+import { Resolvers } from "../../graphql/dist/generated-server";
+
+const path = join(process.cwd(), "graphql", "schema.graphql");
+const typeDefs = readFileSync(path).toString("utf-8");
+
+const users = [
+  { id: "1", name: "Alice" },
+  { id: "2", name: "Bob" },
+  { id: "3", name: "Carol" },
+];
+
+// スキーマと実際のデータ構造の紐付けを resolvers で行う
+const resolvers: Resolvers = {
+  Query: {
+    users: () => users,
+  },
+};
+
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
+
+const startServer = apolloServer.start();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  await startServer;
+  await apolloServer.createHandler({
+    path: "/api/graphql",
+  })(req, res);
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
